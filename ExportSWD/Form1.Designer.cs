bbd7f988fd3de4fd7d8af9e1c40e4868e52a3992
STAROSTWO POWIATOWE PAJĘCZNO <informatyk@powiatpajeczno.pl>
2019-01-25 17:36:48 +0100

    Dodaj pliki projektów.

---
+++ ExportSWD/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ExportSWD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(31, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(387, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Plik konfiguracyjny";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(472, 37);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(438, 334);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ścieżka do EWOPIS-u";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(387, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Validating += new System.ComponentModel.CancelEventHandler(this.textBox2_Validating);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(484, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Twórz pliki wsadowe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.Location = new System.Drawing.Point(31, 504);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(879, 97);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 485);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Komunikaty";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(650, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 52);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Twórz pliki SWD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(937, 632);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ExportSWD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ExportSWD/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+namespace ExportSWD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, string> bazy = new Dictionary<string, string>();
+        
+
+        private string file;
+
+        private string path;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileNames[0];
+                path = Path.GetDirectoryName(openFileDialog1.FileName);
+                using (StreamReader sr = new StreamReader(openFileDialog1.FileNames[0]))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    richTextBox1.Text = line;
+                    file = line;
+                }
+                dodajKomunikat("Wczytano plik konfiguracyjny", Color.Green);
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.ValidateChildren())
+            {
+               
+                foreach (KeyValuePair<string, string> val in bazy)
+                {
+                    replace(val.Key, val.Value);
+                }
+            } else
+            {
+                
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bazy.Add("DZIAŁOSZYN-miasto_ZINTEGROWANA", "1009014");
+            bazy.Add("DZIAŁOSZYN-obszar wiejski", "1009015");
+            bazy.Add("KIEŁCZYGŁÓW", "1009022");
+            bazy.Add("KIEŁCZYGŁÓW_ZINTEGROWANA", "1009022");
+            bazy.Add("NOWA BRZEŹNICA", "1009032");
+            bazy.Add("PAJĘCZNO-miasto", "1009044");
+            bazy.Add("PAJĘCZNO-obszar wiejski", "1009045");
+            bazy.Add("RZĄŚNIA", "1009052");
+            bazy.Add("SIEMKOWICE","1009062");
+            bazy.Add("SIEMKOWICE_ZINTEGROWANA", "1009062");
+            bazy.Add("STRZELCE WIELKIE", "1009072");
+            bazy.Add("SULMIERZYCE", "1009082");
+            ColumnHeader header = new ColumnHeader();
+            header.Text = "";
+            header.Name = "col1";
+            header.Width = 500;
+            listView1.Columns.Add(header);
+           if (Properties.Resources.EWOPIS.Length >0)
+            {
+                textBox2.Text = Properties.Resources.EWOPIS;
+            }
+           
+        }
+
+        private void replace(string nazwa_pliku,string nazwa_bazy)
+        {
+            string plik_swd = nazwa_pliku;
+
+            string output_1 = Regex.Replace(file, @"^EFile=D:\\programy\\ewopis6\\SWDE\\[0-9]{7}\.swd", "EFile="+path+@"\"+nazwa_bazy+"_"+plik_swd+".swd", RegexOptions.Multiline);
+            string output_2 = Regex.Replace(output_1, @"^GminaId\=.*$", @"GminaId="+nazwa_pliku , RegexOptions.Multiline);
+            string output_3 = Regex.Replace(output_2, @"Baza=srv-gndb.powiatpajeczno.local:D:\\bazy\\ewopis\\POWIAT1009\\[0-9]{7}\.fdb", @"Baza=srv-gndb.powiatpajeczno.local:D:\bazy\ewopis\POWIAT1009\"+nazwa_bazy+".fdb", RegexOptions.IgnoreCase);
+            
+            ListViewItem item = new ListViewItem();
+            item.Text = string.Format("Tworzę plik wynikowy dla bazy{0} - gmina {1}", nazwa_bazy, nazwa_pliku);
+            listView1.Items.Add(item);
+            if (!Directory.Exists(path + @"\wsady"))
+            {
+                Directory.CreateDirectory(path + @"\wsady");
+            }
+            using (StreamWriter writer = new StreamWriter(path+@"\wsady\"+plik_swd+".ini", false, Encoding.Unicode))
+            {
+                writer.WriteLine(output_3);
+            }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if (textBox1.Text.Length==0)
+            {
+                errorProvider1.SetError(textBox1, "Wskaż plik konfiguracyjny");
+                e.Cancel = true;
+                dodajKomunikat("Brak wskazanego pliku konfiguracyjnego", Color.Red);
+
+            } else
+            {
+                errorProvider1.SetError(textBox1, "");
+                e.Cancel = false;
+            }
+        }
+
+        private void dodajKomunikat(string komunikat,Color kolor)
+        {
+            
+                ListViewItem item = new ListViewItem();
+                item.Text = komunikat;
+                item.ForeColor = kolor;
+                listView1.Items.Add(item);
+            
+        }
+        private void dodajKomunikat(string komunikat)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = komunikat;
+            item.ForeColor = Color.Black;
+            listView1.Items.Add(item);
+        }
+
+        private void textBox2_Validating(object sender, CancelEventArgs e)
+        {
+            if (textBox2.Text.Length ==0)
+            {
+                errorProvider1.SetError(textBox2, "Brak ścieżki do EWOPIS-u");
+                e.Cancel = true;
+                dodajKomunikat("Brak ścieżki do EWOPIS-u",Color.Red);
+            } else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(textBox2, "");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           string result = Path.GetExtension(path);
+        }
+    }
+}
